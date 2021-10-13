aa576334e8304278737018c2c03e04d47ed2afac
Nebye <nebyeb@hotmail.com>
2021-10-12 19:53:37 -0600

    vec3, color and ray

--- RayTracingRenderer/RayTracingRenderer/Main.cpp
+++ RayTracingRenderer/RayTracingRenderer/Main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+#include "vec3.h"
+#include "color.h"
+
+#include <thread>
+#include <chrono>
+
 int main()
 {
 	/*
@@ -14,14 +20,21 @@ int main()
 	// render
 	std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
+
 	for (int i = (image_height - 1); i >= 0; i--)
 	{
 		// cerr is the error output stream
 		// first part of the progress indicator
 		std::cerr << "\rScanlines remaining: " << i << ' ' << std::flush;
+		
+		// progress bar...a work in progress...
+		//std::cerr << "\rScanlines remaining: " << std::string(((((i / (image_height - 1)) - 1) * 100) * -1), '#') << ' ' << std::flush;
+
+		
 
 		for (int j = 0; j < image_width; j++)
 		{
+			/*
 			auto r = double(j) / (image_width - 1);
 			auto g = double(i) / (image_height - 1);
 			auto b = 0.25;
@@ -31,8 +44,15 @@ int main()
 			int ib = static_cast<int>(255.99 * b);
 
 			std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+			*/
+
+			// new implementation using vec3 and color header files 
+			color pixel_color(double(j) / (image_width - 1), double(i) / (image_height - 1), 0.25);
+			write_color(std::cout, pixel_color);
+			
 		}
 	}
+	
 
 	// end of the progress indicator
 	std::cerr << "\nDone.\n";
---
+++ RayTracingRenderer/RayTracingRenderer/color.h
@@ -0,0 +1,21 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+
+/*
+* Utility Functions 
+* For storing Color
+*/
+
+void write_color(std::ostream& out, color pixel_color) 
+    {
+        // Write the translated [0,255] value of each color component.
+        out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+            << static_cast<int>(255.999 * pixel_color.y()) << ' '
+            << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    }
+
+#endif
